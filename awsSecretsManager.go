77967ca52c59f684223b5c7f228b1f9fbcdc905e
Scott Leggett <scott@sl.id.au>
2021-06-21 23:28:14 +0800

    chore: refactor for extensibility

---
+++ awsSecretsManager.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+// LAGOON_EXTERNAL_SECRETS_AWS_SECRETS_MANAGER must contain a string with this
+// format:
+// <ARN>#<AWS_ACCESS_KEY_ID>#<AWS_SECRET_ACCESS_KEY>
+const asmConfigVar = "LAGOON_EXTERNAL_SECRETS_AWS_SECRETS_MANAGER"
+const asmName = "AWS Secrets Manager"
+
+// These environment variables are accessed by the AWS Go SDK to obtain
+// credentials.
+const awsAccessKeyVar = "AWS_ACCESS_KEY_ID"
+const awsSecretAccessKeyVar = "AWS_SECRET_ACCESS_KEY"
+
+// AWSSecretsManager implements the SecretStore interface for AWS Secrets
+// Manager.
+type AWSSecretsManager struct{}
+
+// Name returns a meaningful name for the secrets store.
+func (s *AWSSecretsManager) Name() string {
+	return asmName
+}
+
+// Secrets returns a map of secret key values.
+func (s *AWSSecretsManager) Secrets() (map[string]string, error) {
+	// find the config variable
+	asmConfig, ok := os.LookupEnv(asmConfigVar)
+	if !ok {
+		log.Printf("no env var %s for secret store %s", asmConfigVar, asmName)
+		return nil, nil
+	}
+	// extract the secret store credentials
+	arn, accessKey, secretAccessKey, region, err := ASMGetCreds(asmConfig)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse %s value: %v", asmConfigVar, err)
+	}
+	// inject credentials into the environment so that the AWS SDK can find them
+	if err = os.Setenv(awsAccessKeyVar, accessKey); err != nil {
+		return nil, fmt.Errorf("couldn't set environment var %v", awsAccessKeyVar)
+	}
+	if err = os.Setenv(awsSecretAccessKeyVar, secretAccessKey); err != nil {
+		return nil, fmt.Errorf("couldn't set environment var %v", awsSecretAccessKeyVar)
+	}
+	// load the AWS SDK configuration, including credentials
+	ctx, cancel := context.WithDeadline(context.Background(),
+		time.Now().Add(30*time.Second))
+	defer cancel()
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithSharedConfigFiles(nil),
+	)
+	cfg.Region = region // SDK requires region separately
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get env config: %v", err)
+	}
+	// get the secret
+	client := secretsmanager.NewFromConfig(cfg)
+	out, err := client.GetSecretValue(ctx,
+		&secretsmanager.GetSecretValueInput{SecretId: &arn})
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get secret value: %v", err)
+	}
+	// extract the secret values
+	secrets := map[string]string{}
+	if err = json.Unmarshal([]byte(*out.SecretString), &secrets); err != nil {
+		return nil, fmt.Errorf("couldn't unmarshal secret values: %v", err)
+	}
+	return secrets, nil
+}
+
+// ASMGetCreds takes a #-separated string containing ARN, API_KEY and
+// API_SECRET_KEY, splits them, and returns those variables. It also parses the
+// region out of the ARN and returns that separately.
+func ASMGetCreds(creds string) (string, string, string, string, error) {
+	parts := strings.Split(creds, "#")
+	if len(parts) != 3 {
+		return "", "", "", "", fmt.Errorf("invalid credential format")
+	}
+	arnParts := strings.Split(parts[0], ":")
+	if len(arnParts) < 7 {
+		return "", "", "", "", fmt.Errorf("invalid credential format")
+	}
+	return parts[0], parts[1], parts[2], arnParts[3], nil
+}
--- main.go
+++ main.go
@@ -1,49 +1,27 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
 	"log"
-	"os"
 	"strings"
-	"time"
-
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
-const awsSecretsManagerVar = "LAGOON_EXTERNAL_SECRETS_AWS_SECRETS_MANAGER"
-const accessKeyVar = "AWS_ACCESS_KEY_ID"
-const secretAccessKeyVar = "AWS_SECRET_ACCESS_KEY"
-
-// ExtractCredentials takes a #-separated string containing ARN, API_KEY and
-// API_SECRET_KEY, splits them, and returns those variables.
-// It also parses the region out of the ARN and returns that separately.
-func ExtractCredentials(creds string) (string, string, string, string, error) {
-	parts := strings.Split(creds, "#")
-	if len(parts) != 3 {
-		return "", "", "", "", fmt.Errorf("invalid credential format")
-	}
-	arnParts := strings.Split(parts[0], ":")
-	if len(arnParts) < 7 {
-		return "", "", "", "", fmt.Errorf("invalid credential format")
-	}
-	return parts[0], parts[1], parts[2], arnParts[3], nil
+// SecretStore represents an external secret store from which secrets can be
+// obtained for use in a Lagoon build.
+type SecretStore interface {
+	// Name returns a meaningful name for the secrets store.
+	Name() string
+	// Secrets returns the secrets available in this secret store as a map of
+	// key-value pairs.
+	// It may return a nil map and nil error if this secret store is not
+	// configured via the associated Lagoon environment variable.
+	Secrets() (map[string]string, error)
 }
 
-// Encode base64-encodes the given string.
-func Encode(i string) (string, error) {
-	b, err := json.Marshal(i)
-	if err != nil {
-		return "", fmt.Errorf(`couldn't marshal "%s": %v`, i, err)
-	}
-	return string(b), nil
-}
-
-// Normalize replaces characters not valid for shell variable names with
+// normalizeShellVar replaces characters not valid for shell variable names with
 // underscores.
-func Normalize(i string) string {
+func normalizeShellVar(i string) string {
 	return strings.Map(func(r rune) rune {
 		switch {
 		case r >= 'A' && r <= 'Z':
@@ -59,53 +37,21 @@ func Normalize(i string) string {
 }
 
 func main() {
-	// LAGOON_EXTERNAL_SECRETS_AWS_SECRETS_MANAGER must contain a string:
-	// <ARN>#<AWS_ACCESS_KEY_ID>#<AWS_SECRET_ACCESS_KEY>
-	creds, ok := os.LookupEnv(awsSecretsManagerVar)
-	if !ok {
-		log.Fatalf("missing env var %s", awsSecretsManagerVar)
+	stores := []SecretStore{
+		&AWSSecretsManager{},
 	}
-	arn, accessKey, secretAccessKey, region, err := ExtractCredentials(creds)
-	if err != nil {
-		log.Fatalf("couldn't parse %s value: %v", awsSecretsManagerVar, err)
-	}
-	// inject credentials into the environment so that the AWS SDK can find them
-	if err = os.Setenv(accessKeyVar, accessKey); err != nil {
-		log.Fatalf("couldn't set environment var %v", accessKeyVar)
-	}
-	if err = os.Setenv(secretAccessKeyVar, secretAccessKey); err != nil {
-		log.Fatalf("couldn't set environment var %v", accessKeyVar)
-	}
-	// load the configuration, including credentials
-	ctx, cancel := context.WithDeadline(context.Background(),
-		time.Now().Add(30*time.Second))
-	defer cancel()
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithSharedConfigFiles(nil),
-	)
-	cfg.Region = region // region is required
-	if err != nil {
-		log.Fatalf("couldn't get env config: %v", err)
-	}
-	// get the secret
-	client := secretsmanager.NewFromConfig(cfg)
-	out, err := client.GetSecretValue(ctx,
-		&secretsmanager.GetSecretValueInput{SecretId: &arn})
-	if err != nil {
-		log.Fatalf("couldn't get secret value: %v", err)
-	}
-	secrets := map[string]string{}
-	if err = json.Unmarshal([]byte(*out.SecretString), &secrets); err != nil {
-		log.Fatalf("couldn't unmarshal secret values: %v", err)
-	}
-	// print some shell code
-	for k, v := range secrets {
-		cleanVal, err := EscapeQuote(v)
+
+	var normalKey string
+	for _, s := range stores {
+		secrets, err := s.Secrets()
 		if err != nil {
-			log.Fatalf(`couldn't escape and quote "%v": %v`, cleanVal, err)
+			log.Fatalf("couldn't retrieve secrets from %s: %v", s.Name(), err)
 		}
-		normalKey := Normalize(k)
-		fmt.Printf("EXTERNAL_SECRET_%s=%s; export EXTERNAL_SECRET_%s;\n", normalKey,
-			cleanVal, normalKey)
+		// TODO split this out for testability
+		for k, v := range secrets {
+			normalKey = normalizeShellVar(k)
+			fmt.Printf("EXTERNAL_SECRET_%s=%s; export EXTERNAL_SECRET_%s;\n",
+				normalKey, base64.StdEncoding.EncodeToString([]byte(v)), normalKey)
+		}
 	}
 }
